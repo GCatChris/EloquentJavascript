7ae690f6396bed3a2bcd6af051bec84f57f699c3
GCatChris <gcatchris@gmail.com>
2015-01-20 17:31:44 -0800

    CharCounter.js
    Updated for indentation errors

--- CharCounter.js
+++ CharCounter.js
@@ -6,8 +6,9 @@ function countBs(str) {
   	var count = 0;
   	// For loop through the length of the string
  	for (var i = 0; i < str.length; i++)
- 	  // If indice of string is B increment the counter variable
-        count++;
+ 	    // If indice of string is B increment the counter variable
+      	    if(str.charAt(i) == "B")
+    			count++;
 	// Return the counter variable  
  	return count; 		
         
@@ -19,9 +20,9 @@ function countChar(str, test) {
   	var count = 0;
   	// For loop through the length of the string
  	for (var i = 0; i < str.length; i++)
-      if(str.charAt(i) == test)
-        // If indice of string is B increment the counter variable
-        count++;
+            if(str.charAt(i) == test)
+            // If indice of string is B increment the counter variable
+                count++;
     // Return the counter variable
  	return count; 		
         
